7900ecae9b048165556ea82b49b9e90cd810b582
Santiago Becerra <santialmancy@gmail.com>
2023-04-18 17:12:11 -0400

    P2-001 Configuration for Development and program.cs modified for dynamic title swagger.

--- PracticeTwo/Program.cs
+++ PracticeTwo/Program.cs
@@ -1,4 +1,5 @@
 using Serilog;
+using Microsoft.OpenApi.Models;
 
 //create the logger and setup your sinks, filters and properties
 Log.Logger = new LoggerConfiguration()
@@ -15,16 +16,38 @@ builder.Host.UseSerilog();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+var configurationBuilder = new ConfigurationBuilder()
+        .SetBasePath(builder.Environment.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
+
+IConfiguration Configuration = configurationBuilder.Build();
+string siteTitle = Configuration.GetSection("Title").Value;
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = siteTitle
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+/*
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+*/
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 

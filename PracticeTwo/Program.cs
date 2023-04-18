0a2ca982d0d6d801ca553d7f23999760f20caed3
Santiago Becerra <santialmancy@gmail.com>
2023-04-18 16:50:16 -0400

    P2-001 Modified program.cs for SERILOG.

--- PracticeTwo/Program.cs
+++ PracticeTwo/Program.cs
@@ -1,4 +1,14 @@
+using Serilog;
+
+//create the logger and setup your sinks, filters and properties
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
 var builder = WebApplication.CreateBuilder(args);
+
+//after create the builder - UseSerilog
+builder.Host.UseSerilog();
 
 // Add services to the container.
 
